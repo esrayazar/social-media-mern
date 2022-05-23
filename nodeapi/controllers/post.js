49456fbdc43c733f058de570e860d2d20c2993f0
Esra Yazar <esrayzr@gmail.com>
2022-05-23 10:39:05 -0400

    implemented get all posts by user

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 
 exports.getPosts = (req, res)=>{
    const posts = Post.find()
+   .populate("postedBy", "_id name")
    .select("_id title body")
    .then((posts)=>{
        res.json({posts})
@@ -39,4 +40,18 @@ exports.createPost = (req, res)=>{
         });
         });
 
+};
+exports.postsByUser =(req, res) =>{
+    Post.find({postedBy: req.profile._id})
+    .populate("postedBy", "_id name")
+    .sort("_created")
+    .exec((err, posts)=>{
+        if(err){
+            return res.status(400).json({
+                error: err
+            });
+        }
+        res.json(posts);
+
+    });
 };
