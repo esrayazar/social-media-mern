ea7fb33a4a84a44665bafcb0b4140695aaa2a4f6
Esra Yazar <esrayzr@gmail.com>
2022-05-23 14:04:32 -0400

    deleting and updating post implemented

--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {getPosts, createPost, postsByUser} = require('../controllers/post');
+const {getPosts, createPost, postsByUser, postById, isPoster, updatePost, deletePost} = require('../controllers/post');
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const {createPostValidator} = require('../validator')
@@ -9,7 +9,15 @@ const router =  express.Router();
 router.get("/", getPosts )
 router.post("/post/new/:userId", requireSignin, createPost, createPostValidator )
 router.get("/posts/by/:userId", postsByUser);
+router.put("/post/:postId", requireSignin, isPoster, updatePost)
+router.delete("/post/:postId", requireSignin, isPoster, deletePost);
+
+
+
+
 //any route containing : userId, our app will first execute userById()
 router.param("userId", userById)
+//any route containing : userId, our app will first execute postById()
+router.param("postId", postById)
 
 module.exports = router;
