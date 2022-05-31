11ea2a775c0a2c87f4777e8ba047cfb53b79a986
Esra Yazar <esrayzr@gmail.com>
2022-05-31 13:30:21 -0400

    signout func added and styled

--- react-social/src/core/Menu.js
+++ react-social/src/core/Menu.js
@@ -5,6 +5,18 @@ const isActive = (history, path) =>{
     if(history.location.pathname === path) return {color: "#ff9900"}
         else return {color: "#ffffff"}
 }
+export const signout = next => {
+    if (typeof window !== "undefined") localStorage.removeItem("jwt");
+    next();
+    return fetch("http://localhost:8080/signout", {
+        method: "GET"
+    })
+        .then(response => {
+        console.log('signout', response)
+        return response.json()
+    })
+    .catch(err => console.log(err));
+}
 const Menu =({history}) =>(
     <div>
        <ul className="nav nav-tabs bg-primary">
@@ -28,6 +40,19 @@ const Menu =({history}) =>(
                 >
                     Sign Up
                 </Link>
+            </li>
+
+            <li className="nav-item">
+                <a
+                    className= "nav-link" 
+                    style={
+                        (isActive(history, "/signup"), 
+                        {cursor: "pointer", color: "#fff"})
+                    }
+                    onClick = {() => signout(() => history.push('/'))}
+                >
+                    Sign Out
+                </a>
             </li>
             
         </ul>
