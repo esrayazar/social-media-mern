202fd622f56a6d7c9ed8986ba005ca67b2ec77ce
Esra Yazar <esrayzr@gmail.com>
2022-05-31 12:06:21 -0400

    added sigin and signup routes

--- react-social/src/MainRouter.js
+++ react-social/src/MainRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route, Switch } from 'react-router-dom';
 import Home from "./core/Home";
+import Signin from './user/Signin';
 import Signup from './user/Signup';
 
 const MainRouter = () =>(
@@ -8,6 +9,7 @@ const MainRouter = () =>(
         <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/signup" component={Signup}/>
+            <Route exact path="/signin" component={Signin}/>
         </Switch>
     </div>
 )
