96f1025d1a8443e275278e75b809b1c080d9cad7
Esra Yazar <esrayzr@gmail.com>
2022-05-31 12:13:11 -0400

    menu bar added

---
+++ react-social/src/core/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Menu =() =>(
+    <div>
+        <Link to="/">Home</Link>
+        <Link to="/signin">Sign In</Link>
+        <Link to="/signup">Sign Up</Link>
+    </div>
+);
+export default Menu;
