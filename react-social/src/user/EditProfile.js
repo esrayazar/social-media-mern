5355f15215f4063aa76ca6ddc43b033da36cb1a1
Esra Yazar <esrayzr@gmail.com>
2022-06-02 19:25:48 -0400

    loading implemented on editprofile page

--- react-social/src/user/EditProfile.js
+++ react-social/src/user/EditProfile.js
@@ -13,8 +13,10 @@ import {read, update} from "./apiUser";
             email: "",
             password: "",
             redirectToProfile:false,
-            error: ""
-        }
+            error: "",
+            fileSize: 0,
+            loading: false
+        };
     }
 
     init =(userId) =>{
@@ -36,13 +38,14 @@ import {read, update} from "./apiUser";
     };
 
     componentDidMount(){
+        this.userData = new FormData()
         const userId = this.props.match.params.userId;
         this.init(userId);
     }
 
     isValid = () => {
         const {name, email, password} =this.state
-        if(name.length ==0){
+        if(name.length ===0){
             this.setState({error:" Name is required"})
             return false
         }
@@ -61,23 +64,20 @@ import {read, update} from "./apiUser";
     }
 
     handleChange =(name) => (event)=>{
-        this.setState({[name]:event.target.value});
+        const value =name === 'photo' ? event.target.files[0] : event.target.value
+        this.userData.set(name, value)
+        this.setState({[name]: value});
 
     };
     clickSubmit = event =>{
         event.preventDefault()
+        this.setState({loading:true})
+
         if(this.isValid()){
-        const{name, email, password} = this.state;
-        const user ={
-            name,
-            email,
-            password: password || undefined
-        };
-        //console.log(user)
         const userId = this.props.match.params.userId;
         const token = isAuthenticated().token
 
-        update(userId, token, user).then(data =>{
+        update(userId, token, this.userData).then(data =>{
            if(data.error) this.setState({error: data.error})
            else this.setState({
               redirectToProfile:true
@@ -89,6 +89,14 @@ import {read, update} from "./apiUser";
 
     signupForm = (name, email,password)=>(
         <form>
+            <div className="form-group">
+            <label className="text-muted">Profile Photo</label>
+            <input 
+            onChange={this.handleChange("photo")} 
+            type="file" 
+            accept='image/*'
+            className="form-control"/>
+        </div>
         <div className="form-group">
             <label className="text-muted">Name</label>
             <input onChange={this.handleChange("name")} type="text" className="form-control" value={name}/>
@@ -107,7 +115,7 @@ import {read, update} from "./apiUser";
     )
 
   render() {
-    const { id,name, email,password, redirectToProfile, error} =this.state
+    const { id,name, email,password, redirectToProfile, error,loading} =this.state
 
     if(redirectToProfile){
        return <Redirect to={`/user/${id}`}/>
@@ -122,6 +130,13 @@ import {read, update} from "./apiUser";
                 > 
                 {error}
                 </div>
+                {loading ? (
+                <div className="jumbotron text-center"> 
+                    <h2> Loading...</h2>
+                </div>
+                ):(
+                    ""
+                )}
 
           {this.signupForm(name, email,password)}
       </div>
