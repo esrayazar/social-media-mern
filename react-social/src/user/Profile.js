4c2d412963f8ee1894a437b24636e629daf4867e
Esra Yazar <esrayzr@gmail.com>
2022-06-01 21:50:04 -0400

    implemented delete user functionalities

--- react-social/src/user/Profile.js
+++ react-social/src/user/Profile.js
@@ -3,6 +3,7 @@ import { Redirect, Link } from 'react-router-dom';
 import { isAuthenticated } from '../auth';
 import {read} from './apiUser';
 import DefaultProfile from '../images/profilepic.jpg'
+import DeleteUser from './DeleteUser';
 
 class Profile extends Component{
     constructor(){
@@ -32,6 +33,14 @@ class Profile extends Component{
         this.init(userId);
         
     }
+
+    componentWillReceiveProps(props){
+        const userId = props.match.params.userId;
+        this.init(userId);
+        
+    }
+
+    
     render(){
 
         const {redirectToSignin, user} = this.state
@@ -62,13 +71,13 @@ class Profile extends Component{
                    ).toDateString()}`}</p>
                </div>
                    {isAuthenticated().user && 
-                   isAuthenticated().user._id ==user._id &&(
+                   isAuthenticated().user._id ===user._id &&(
                        <div className="d-inline-block"> 
                             <Link className='btn btn-raised btn-success mr-5' to= {`/user/edit/${user._id}`}
                             >
                                 Edit Profile
                             </Link>
-                            <button className="btn btn-raised btn-danger">Delete Profile</button>
+                           <DeleteUser userId={user._id} />
                        </div>
                    )}
                 </div>
