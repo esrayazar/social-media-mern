050ef97c54a477075d7776c3c5f1694002d6dd9a
Esra Yazar <esrayzr@gmail.com>
2022-06-03 10:33:46 -0400

    display following user

--- react-social/src/user/ProfileTabs.js
+++ react-social/src/user/ProfileTabs.js
@@ -13,11 +13,17 @@ class ProfileTabs extends Component {
                  <hr/>
                  {followers.map((person, i) =>
                       <div key={i}> 
-                        <div className="row">
+                      
                             <div>
                                 <Link to={`/user/${person._id}`}>
-                                    <img className="float-left mr-2"
+                                <img
+                                    style={{
+                                        borderRadius: "50%",
+                                        border: "1px solid black"
+                                    }} 
+                                    className="float-left mr-2"
                                     height="30px"
+                                    width="30px"
                                      onError= {i =>(i.target.src =`${DefaultProfile}`)}
                                     src={`${process.env.REACT_APP_API_URL}/user/photo/${person._id}`}
                                     alt={person.name}
@@ -26,19 +32,45 @@ class ProfileTabs extends Component {
                                         {person.name}
                                     </div>
                                 </Link>
-                                <p style={{clear: 'both'}}>
-                                    {person.about}
-                                </p>
                             </div>
                         </div>
-                     </div>
                  )}
+             </div>  
+
+             <div className="col-md-4">
+                 <h3 className="text-primary"> Following</h3>
+                 <hr/>
+                 {following.map((person, i) =>
+                      <div key={i}> 
+                            <div>
+                                <Link to={`/user/${person._id}`}>
+                                    <img
+                                    style={{
+                                        borderRadius: "50%",
+                                        border: "1px solid black"
+                                    }} 
+                                    className="float-left mr-2"
+                                    height="30px"
+                                    width="30px"
+                                     onError= {i =>(i.target.src =`${DefaultProfile}`)}
+                                    src={`${process.env.REACT_APP_API_URL}/user/photo/${person._id}`}
+                                    alt={person.name}
+                                    />
+                                    <div >
+                                       <p className="lead">{person.name}</p> 
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                 )}
+             </div> 
+             <div className="col-md-4">
+             <h3 className="text-primary"> Posts</h3>
+                 <hr/>
              </div>
-            
          </div>
-         
       </div>
-    )
+    );
   }
 }
 
