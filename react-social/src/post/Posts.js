6f4d59997c1c7ecf38bfc28706e519333ce07ddc
Esra Yazar <esrayzr@gmail.com>
2022-06-05 23:50:12 -0400

    show posts with image

--- react-social/src/post/Posts.js
+++ react-social/src/post/Posts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {list} from './apiPost';
-//import DefaultProfile from '../images/profilepic.jpg'
+import DefaultPost from '../images/mountains.jpg'
 import { Link } from 'react-router-dom';
 
  class Posts extends Component {
@@ -33,6 +33,11 @@ import { Link } from 'react-router-dom';
                 return (
                     <div className="card col-md-4"  key={i}>
                     <div className= "card-body">
+                      <img src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`} alt={post.title}
+                      onError={i => i.target.src = `${DefaultPost}`}
+                      className="img-thunbnail mb-3"
+                      style={{height: "200px", width: "auto"}}
+                      />
                       <h5 className="card-title"> {post.title}</h5>
                       <p className="card-text">{post.body.substring(0,100)}</p>
                       <br/>
