93441ed60a49903dbbbe06580e7c954c984118b5
Esra Yazar <esrayzr@gmail.com>
2022-06-05 22:42:49 -0400

    new post route created

--- react-social/src/MainRouter.js
+++ react-social/src/MainRouter.js
@@ -8,6 +8,7 @@ import Profile from './user/Profile';
 import Users from './user/Users';
 import EditProfile from './user/EditProfile';
 import FindPeople from './user/FindPeople';
+import NewPost from './post/NewPost'
 import PrivateRoute from './auth/PrivateRoute';
 
 
@@ -22,6 +23,7 @@ const MainRouter = () =>(
             <PrivateRoute exact path="/user/edit/:userId" component={EditProfile}/>
             <PrivateRoute exact path="/findpeople" component={FindPeople}/>
             <PrivateRoute exact path="/user/:userId" component={Profile}/>
+            <PrivateRoute exact path="/post/create" component={NewPost}/>
         </Switch>
     </div>
 )
