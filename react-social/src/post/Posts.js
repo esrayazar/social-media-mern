d65bdf76ce07f2de7605b2f1cf6e6a003ef6aa6f
Esra Yazar <esrayzr@gmail.com>
2022-06-05 23:33:59 -0400

    show posts user date implemented

--- react-social/src/post/Posts.js
+++ react-social/src/post/Posts.js
@@ -19,29 +19,44 @@ import { Link } from 'react-router-dom';
        }
      })
    }
-   renderPosts = posts =>(
-    <div className="row">
-      {posts.map((post, i) =>(
-        <div className="card col-md-4"  key={i}>
-        {/* <img 
-        style={{height: "200px", width:"auto"}}
-        className="img-thumbnail"
-        src={`${process.env.REACT_APP_API_URL}/user/photo/${user._id}`} 
-        onError= {i =>(i.target.src =`${DefaultProfile}`)}
-        alt={user.name}    
-        /> */}
-        <div className= "card-body">
-          <h5 className="card-title"> {post.title}</h5>
-          <p className="card-text">{post.body}</p>
-          <Link
-          to= {`/posts/${post._id}`} 
-          className= "btn btn-raised btn-primary btn-small">Read More</Link>
-        </div>
+   renderPosts = posts =>{
+    return (
+        <div className="row">
+            {posts.map((post, i) => {
+                const posterId = post.postedBy
+                    ? `/user/${post.postedBy._id}`
+                    : "";
+                const posterName = post.postedBy
+                    ? post.postedBy.name
+                    : " Unknown";
 
-        </div>
-      ))}
-    </div>
-   );
+                return (
+                    <div className="card col-md-4"  key={i}>
+                    <div className= "card-body">
+                      <h5 className="card-title"> {post.title}</h5>
+                      <p className="card-text">{post.body.substring(0,100)}</p>
+                      <br/>
+                      <p className="font-italic mark">
+                                    Posted by{" "}
+                                    <Link to={`${posterId}`}>
+                                        {posterName}{" "}
+                                    </Link>
+                                    on {new Date(post.created).toDateString()}
+                                </p>
+                                <Link
+                                    to={`/post/${post._id}`}
+                                    className="btn btn-raised btn-primary btn-sm"
+                                >
+                                    Read more
+                                </Link>
+                      
+                    </div>
+                 </div>
+                 )
+             })}
+      </div>
+       )
+   };
 
   render() {
     const {posts} = this.state;
