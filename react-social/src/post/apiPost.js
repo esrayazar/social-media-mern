8dbf7a0bcdf80a58ba957f9652c6235321df4747
Esra Yazar <esrayzr@gmail.com>
2022-06-06 09:13:51 -0400

    minor fixes

--- react-social/src/post/apiPost.js
+++ react-social/src/post/apiPost.js
@@ -24,15 +24,27 @@ export const list =() =>{
     .catch(err => console.log(err));
 
  }
-
- export const singlePost =(postId) =>{
-    return fetch(`${process.env.REACT_APP_API_URL}/post/${postId}`,{
-        method: "GET",
-        }
-    )
-    .then(response => {
-        return response.json()
+ export const singlePost = postId => {
+    return fetch(`${process.env.REACT_APP_API_URL}/post/${postId}`, {
+        method: "GET"
     })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
 
- }
+export const listByUser = (userId, token) => {
+    return fetch(`${process.env.REACT_APP_API_URL}/posts/by/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
