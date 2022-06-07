d2ed08e334cbf8823b4f7a692acdad42e0cd098c
Esra Yazar <esrayzr@gmail.com>
2022-06-07 09:56:42 -0400

    minor update

--- react-social/src/post/Posts.js
+++ react-social/src/post/Posts.js
@@ -1,47 +1,72 @@
-import React, { Component } from 'react';
-import {list} from './apiPost';
-import DefaultPost from '../images/mountains.jpg'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { list } from "./apiPost";
+import DefaultPost from "../images/mountains.jpg";
+import { Link } from "react-router-dom";
 
- class Posts extends Component {
-   constructor(){
-     super();
-     this.state = {
-       posts: []
-     };
-   }
-   componentDidMount(){
-     list().then(data =>{
-       if(data.error){
-         console.log(data.error)
-       }else{
-         this.setState({posts:data})
-       }
-     })
-   }
-   renderPosts = posts =>{
-    return (
-        <div className="row">
-            {posts.map((post, i) => {
-                const posterId = post.postedBy
-                    ? `/user/${post.postedBy._id}`
-                    : "";
-                const posterName = post.postedBy
-                    ? post.postedBy.name
-                    : " Unknown";
+class Posts extends Component {
+    constructor() {
+        super();
+        this.state = {
+            posts: [],
+            page: 1
+        };
+    }
 
-                return (
-                    <div className="card col-md-4"  key={i}>
-                    <div className= "card-body">
-                      <img src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`} alt={post.title}
-                      onError={i => i.target.src = `${DefaultPost}`}
-                      className="img-thunbnail mb-3"
-                      style={{height: "200px", width: "100%"}}
-                      />
-                      <h5 className="card-title"> {post.title}</h5>
-                      <p className="card-text">{post.body.substring(0,100)}</p>
-                      <br/>
-                      <p className="font-italic mark">
+    loadPosts = page => {
+        list(page).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                this.setState({ posts: data });
+            }
+        });
+    };
+
+    componentDidMount() {
+        this.loadPosts(this.state.page);
+    }
+
+    loadMore = number => {
+        this.setState({ page: this.state.page + number });
+        this.loadPosts(this.state.page + number);
+    };
+
+    loadLess = number => {
+        this.setState({ page: this.state.page - number });
+        this.loadPosts(this.state.page - number);
+    };
+
+    renderPosts = posts => {
+        return (
+            <div className="row">
+                {posts.map((post, i) => {
+                    const posterId = post.postedBy
+                        ? `/user/${post.postedBy._id}`
+                        : "";
+                    const posterName = post.postedBy
+                        ? post.postedBy.name
+                        : " Unknown";
+
+                    return (
+                        <div className="card col-md-4" key={i}>
+                            <div className="card-body">
+                                <img
+                                    src={`${
+                                        process.env.REACT_APP_API_URL
+                                    }/post/photo/${post._id}`}
+                                    alt={post.title}
+                                    onError={i =>
+                                        (i.target.src = `${DefaultPost}`)
+                                    }
+                                    className="img-thunbnail mb-3"
+                                    style={{ height: "200px", width: "100%" }}
+                                />
+                                <h5 className="card-title">{post.title}</h5>
+                                <p className="card-text">
+                                    {post.body.substring(0, 100)}
+                                </p>
+                                <br />
+                                <p className="font-italic mark">
                                     Posted by{" "}
                                     <Link to={`${posterId}`}>
                                         {posterName}{" "}
@@ -54,25 +79,48 @@ import { Link } from 'react-router-dom';
                                 >
                                     Read more
                                 </Link>
-                      
-                    </div>
-                 </div>
-                 )
-             })}
-      </div>
-       )
-   };
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    };
 
-  render() {
-    const {posts} = this.state;
-    return (
-      <div className="container">
-          <h2 className="mt-5 mb-5">
-            {!posts.length ? "Loading.." : "Recent Posts"}</h2>
+    render() {
+        const { posts, page } = this.state;
+        return (
+            <div className="container">
+                <h2 className="mt-5 mb-5">
+                    {!posts.length ? "No more posts!" : "Recent Posts"}
+                </h2>
 
-          {this.renderPosts(posts)}
-      </div>
-    )
-  }
+                {this.renderPosts(posts)}
+
+                {page > 1 ? (
+                    <button
+                        className="btn btn-raised btn-warning mr-5 mt-5 mb-5"
+                        onClick={() => this.loadLess(1)}
+                    >
+                        Previous ({this.state.page - 1})
+                    </button>
+                ) : (
+                    ""
+                )}
+
+                {posts.length ? (
+                    <button
+                        className="btn btn-raised btn-success mt-5 mb-5"
+                        onClick={() => this.loadMore(1)}
+                    >
+                        Next ({page + 1})
+                    </button>
+                ) : (
+                    ""
+                )}
+            </div>
+        );
+    }
 }
-export default Posts
+
+export default Posts;
